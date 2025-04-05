d284146bb1e034aba51f64d8e93c90420ce52ab2
Valery Melou <me@valerymelou.com>
2025-04-05 19:20:46 +0100

    chore: move core json api library to libs/core

---
+++ libs/jsonapi/core/README.md
@@ -0,0 +1,11 @@
+# json-api
+
+This library was generated with [Nx](https://nx.dev).
+
+## Building
+
+Run `nx build json-api` to build the library.
+
+## Running unit tests
+
+Run `nx test json-api` to execute the unit tests via [Jest](https://jestjs.io).
--- libs/jsonapi/core/jest.config.ts
+++ libs/jsonapi/core/jest.config.ts
@@ -1,10 +1,10 @@
 export default {
-  displayName: 'jsonapi',
-  preset: '../../jest.preset.js',
+  displayName: 'json-api',
+  preset: '../../../jest.preset.js',
   testEnvironment: 'node',
   transform: {
     '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../coverage/libs/jsonapi',
+  coverageDirectory: '../../../coverage/libs/jsonapi/core',
 };
